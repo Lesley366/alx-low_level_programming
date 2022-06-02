7a73e271ddc553c7d8ca15384aa3c7b5142eb6ee
Lesley366 <lesleytalana@gmail.com>
2022-06-02 12:37:10 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: 0 after printing the function 
+ * Return: 0 after printing thr function
  */
 int main(void)
 {
