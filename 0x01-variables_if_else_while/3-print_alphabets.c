f5f95c2f02ee609be4bcb6953f6a3dac4dbbb3b7
Lesley366 <lesleytalana@gmail.com>
2022-06-03 12:13:25 -0700

    lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int ch;
+	int ph;
+
+	for (ch = 'a' ; ch = 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ph = 'A' ; ph = 'Z' ; ch++)
+	{
+		putchar(ph);
+	}
+
+	putchar('\n');
+
+return (0);
+}
