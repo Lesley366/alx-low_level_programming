2b61a0964aa499a27958a601c9313310dacb220d
Lesley366 <lesleytalana@gmail.com>
2022-06-07 14:43:27 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - return 1,0,-1 if n is greater,equal or less
+ * @n: int to be printed
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
