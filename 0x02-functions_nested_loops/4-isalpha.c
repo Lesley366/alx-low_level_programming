f29472995ac44e139b06fcbc6bf1c558002af6c9
Lesley366 <lesleytalana@gmail.com>
2022-06-07 14:15:02 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _aslapha - Returns 1 if c is a letter, lowercase or uppercase
+ * @c: int printed
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}	
