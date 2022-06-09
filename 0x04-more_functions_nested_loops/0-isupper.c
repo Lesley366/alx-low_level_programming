48b490fe5342638adde26fe8255e898909925bb8
Lesley366 <lesleytalana@gmail.com>
2022-06-09 11:38:04 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * i_supper - checks for uppercase character
+ * @c: integer
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
