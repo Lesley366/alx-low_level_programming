33d4fd2d6d71807ecce3be115a74918d6abbf1b0
Lesley366 <lesleytalana@gmail.com>
2022-06-13 09:46:55 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - Write a function that takes a pointer to an int as parameter and updates the value it points to to 98
- * @n - a pointer to an integer
+ * reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98
+ *
+ * @n: integer pointer
+ *
  * Return: 0 Success
  */
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/1-swap.c
+++
@@ -1,3 +0,0 @@
-#include "main.h"
-/**
- * swap_int - 
