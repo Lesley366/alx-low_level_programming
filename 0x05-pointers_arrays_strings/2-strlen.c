0bfe9f005b707ca86ee26cc526bd658cff96c086
Lesley366 <lesleytalana@gmail.com>
2022-06-13 12:47:39 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int _strlen - returns the length of a string
+ * @s: string
+ * Return: 0 Success
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*(s + lenghth) != '\0')
+		length++;
+	return length;
+}
