d4f58cdd1179a1bd731f70e711af8a51b13382e5
Lesley366 <lesleytalana@gmail.com>
2022-06-16 11:58:06 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0;
+	int i;
+	int j;
 
-	while (count >= 0)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		*(dest + count) = *(src + count);
-
-		if (*(src + count) == '\0')
-			break;
-		count++;
+		i++;
 	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
