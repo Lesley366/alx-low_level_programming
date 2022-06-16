c58c84c51e5be1be34c924495896e43f2f5f6525
Lesley366 <lesleytalana@gmail.com>
2022-06-16 12:36:24 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, p = 0;
+
+	while (p == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		p = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (p);
+}
