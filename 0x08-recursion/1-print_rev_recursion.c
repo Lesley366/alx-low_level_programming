bf018ceed52a39e8683cedc8d97b4508981357b7
Lesley366 <lesleytalana@gmail.com>
2022-06-22 12:51:54 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s - 1);
+	}
+	else
+		_putchar('\n');
+}
