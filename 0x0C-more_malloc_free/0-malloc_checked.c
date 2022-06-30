e07036e8508e4738b2b742c740daae20374c7207
Lesley366 <lesleytalana@gmail.com>
2022-06-30 11:59:10 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked -  allocates memory using malloc
+ * @b: memory
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
