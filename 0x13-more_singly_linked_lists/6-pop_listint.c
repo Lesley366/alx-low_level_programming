edb5dbffc667332982328d1eabca4fdaa683539f
Lesley366 <lesleytalana@gmail.com>
2022-07-20 10:10:51 -0700

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,14 +8,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *current;
+	listint_t *curr;
 
 	if (head != NULL)
 	{
-		current = *head;
-		while ((temp = current) != NULL)
+		curr = *head;
+		while ((temp = curr) != NULL)
 		{
-			current = current->next;
+			curr = curr->next;
 			free(temp);
 		}
 		*head = NULL;
